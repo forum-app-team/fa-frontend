e8513ff5635793602e40407029fa9a9db3b9a599
Xintian <xli239@hawk.iit.edu>
2025-09-15 00:10:19 -0400

    SCRUM-27: integrate mutations to the component, add confirmation modal

---
+++ src/features/admin/components/ConfirmationPopup.jsx
@@ -0,0 +1,32 @@
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+export default function ConfirmationPopup({ show, onHide, bodyContent, handlerRef }) {
+  const handleClick = async () => {
+    try {
+        await handlerRef.current();
+        onHide();
+    } catch (err) {
+        console.error(err);
+    }
+  }
+  return (
+    <Modal
+      show={show}
+      onHide={onHide}
+      backdrop="static"
+      keyboard={false}
+    >
+      <Modal.Header closeButton>
+        {/*<Modal.Title>Modal title</Modal.Title>*/}
+      </Modal.Header>
+      <Modal.Body> {bodyContent}</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onHide}>
+          Cancel
+        </Button>
+        <Button variant="primary" onClick={handleClick}>Confirm</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
