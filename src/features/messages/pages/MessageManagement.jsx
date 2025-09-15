836be8abfa4902d6647de8b43e1c08cd3a90017b
ChanradyHo <cradyh.wk@gmail.com>
2025-09-15 09:54:52 -0700

    SCRUM-23: integrated Contact Admin with message service (#8)
    * feat/SCRUM-23: integrated Contact Admin with message service

--- src/features/messages/pages/MessageManagement.jsx
+++ src/features/messages/pages/MessageManagement.jsx
@@ -1,5 +1,31 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getMessagesThunk } from "../store/message.thunk";
+import { selectMessages } from "../store/message.slice";
+import { MessageList } from "../components/MessageList";
+
 const MessageManagement = () => {
-  return <h1>Message Management</h1>;
+  const dispatch = useDispatch();
+  const messages = useSelector(selectMessages);
+
+  useEffect(() => {
+    dispatch(getMessagesThunk());
+  }, [dispatch]);
+
+  return (
+    <div className="container-fluid py-4">
+      <h1 className="h3">Message Management</h1>
+
+      <div className="card">
+        <div className="card-header">
+          <h5 className="card-title mb-0">All Messages ({messages.length})</h5>
+        </div>
+        <div className="card-body p-0">
+          <MessageList messages={messages} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MessageManagement;
