52813ddc4ec3fb974b78d792aeb5f9eb5be8f8d0
Xintian <xli239@hawk.iit.edu>
2025-09-18 09:48:32 -0400

    SCRUM-48: some ui polishment

--- src/features/users/components/EditProfileButton.jsx
+++ src/features/users/components/EditProfileButton.jsx
@@ -1,12 +1,10 @@
+import { Button } from "react-bootstrap";
+
 const EditProfileButton = ({ onClick }) => {
   return (
-<<<<<<< HEAD
-    <button type="button" onClick={onClick}>
+    <Button variant="primary" onClick={onClick}>
       Edit Profile
-    </button>
-=======
-    <button type="button" onClick={onClick}>Edit Profile</button>
->>>>>>> cd7235898d43c365cb4d7f19ed0f3a98e66dab5a
+    </Button>
   );
 };
 
